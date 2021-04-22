067ec4943598d0bab35c36211db59674ab827ede
helei <2215694729@qq.com>
2021-04-22 14:48:16 +0800

    自动装配Bean,添加git忽略文件路径

--- springcore/stereo-autoconfig/src/main/java/org/spring4combat/config/StereoConfig.java
+++ springcore/stereo-autoconfig/src/main/java/org/spring4combat/config/StereoConfig.java
@@ -1,6 +1,5 @@
 package org.spring4combat.config;
 
-import org.junit.Test;
 import org.spring4combat.service.impl.CDPlayer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,11 +7,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("org.spring4combat.**")
-public class StereConfig {
+public class StereoConfig {
     @Autowired
     private CDPlayer cdPlayer;
 
-    @Test
     public void play(){
         cdPlayer.play();
     }
--- springcore/stereo-autoconfig/src/test/java/CDPlayerTest.java
+++ springcore/stereo-autoconfig/src/test/java/CDPlayerTest.java
@@ -1,9 +1,27 @@
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.spring4combat.config.StereoConfig;
+import org.spring4combat.service.CompactDisc;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {org.spring4combat.config.StereConfig.class})
+@ContextConfiguration(classes = {StereoConfig.class})
 public class CDPlayerTest {
 
+    @Autowired
+    private CompactDisc compactDisc;
+
+    @Test
+    public void cdShouldNotBeNull() {
+        System.out.println("充当日志：cdShouldNotBeNull called...");
+        assertNotNull(compactDisc);
+    }
 }
+/*
+这个测试结果就是：一片空白，因为断言cd不为null，所以条件满足；
+如果cd为null,则抛出AssertionError异常；
+ */
